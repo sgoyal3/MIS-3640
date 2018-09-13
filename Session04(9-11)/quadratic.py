6434218cb16175579addde6d94a2bdcd7cc16cb2
Siddhanth Goyal <sgoyal3@babson.edu>
2018-09-12 23:58:07 -0400

    Quadratic-Session 4

--- Session04(9-11)/quadratic.py
+++ Session04(9-11)/quadratic.py
@@ -1,4 +1,5 @@
 import math
+from math import sqrt #I don't know why import math won't work, but apparently I needed to add this. 
 
 a = float(input("a: "))
 b = float(input("b: "))
@@ -7,15 +8,12 @@ c = float(input("c: "))
 r = (b**2) - (4*a*c)
 
 if r > 0:
-    num_roots = 2
     x1 = (((-b) + sqrt(r))/(2*a))
     x2 = (((-b) - sqrt(r))/(2*a))
     print("Are 2 roots: %f and %f" % (x1, x2))
 elif r == 0: #use an elif statement here, though we haven't technically covered it yet
-    num_roots = 1
     x = (-b) / 2*a
     print("There is one root: ", x)
 else:
-    num_roots = 0
     print("No roots, discriminant is < 0.")
 
