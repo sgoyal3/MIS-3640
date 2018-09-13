7fbc97f0415a7090e5f252c6033e86ce624e7200
Siddhanth Goyal <sgoyal3@babson.edu>
2018-09-12 23:40:30 -0400

    Quadratic-Session04

--- Session04(9-11)/quadratic.py
+++ Session04(9-11)/quadratic.py
@@ -1,11 +1,10 @@
 import math
 
-print("Quadratic function : (a * x^2) + b*x + c")
 a = float(input("a: "))
 b = float(input("b: "))
 c = float(input("c: "))
 
-r = b**2 - 4*a*c
+r = (b**2) - (4*a*c)
 
 if r > 0:
     num_roots = 2
